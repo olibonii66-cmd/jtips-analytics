e900d087e45473d16bd05a7567448805fa27c98a
olibonii66-cmd <olibonii66@gmail.com>
2026-05-28 14:10:47 -0300

    Delete api directory

--- api/ligas.js
+++
@@ -1,3 +0,0 @@
-
-const ALLOWED_LEAGUES = ['BRAZIL 1','ENGLAND 1','FRANCE 1','GERMANY 1','ITALY 1','PORTUGAL 1','SPAIN 1'];
-export default async function handler(req, res) { res.status(200).json({ ok: true, fonte: 'footystats', ligas: ALLOWED_LEAGUES }); }
