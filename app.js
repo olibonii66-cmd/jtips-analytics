2b978f9f6af474a73ab5d576d94038100df8b175
olibonii66-cmd <olibonii66@gmail.com>
2026-06-10 11:53:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+  <rect width="64" height="64" rx="16" fill="#00c853"/>
+  <path d="M13 40 25 28l9 9 17-18" fill="none" stroke="#07120b" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M39 19h12v12" fill="none" stroke="#07120b" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
